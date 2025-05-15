5520a3ed0a8f4289e3071bfdd69f061dbe6789e0
abhisk1 <abhishekskarabani23@gmail.com>
2025-05-15 19:07:25 +0000

    your message here

--- basics-js/01_Variables.js
+++ basics-js/01_Variables.js
@@ -1,1 +1,11 @@
-var customerID = 12344;
+const customerID = 12344;
+let customerName = "John Doe";
+var customerPassword = "password123";
+let customerEmail = "jhon@gmail.com";
+
+
+ customerID = 12345; 
+// not allowed js
+
+
+console.log(customerID); 
