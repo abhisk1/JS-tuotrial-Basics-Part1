aaf4ce106b6f3d2c055a5cd088d8703ab859ea3d
abhisk1 <abhishekskarabani23@gmail.com>
2025-05-21 05:29:34 +0000

    function-01

--- 01_typescript/04_function.ts
+++ 01_typescript/04_function.ts
@@ -1,10 +1,12 @@
 //type interface is not good in case of functions and large codebases. So any is not allowed in strict mode.
 
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
+
+  // return "Hello";
 }
 
-function getUpper(val: string) {
+function getUpper(val: string): string {
   return val.toUpperCase();
 }
 
@@ -16,9 +18,33 @@ function signUp(name: string, email: string, isPaid: boolean) {
   };
 }
 
-addTwo(5); // 7
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+  return {
+    name,
+    email,
+    isPaid,
+  };
+};
+
+console.log(addTwo(5)); // 7
 getUpper("Hello"); // "HELLO"
 
 const result = signUp("John", "jhon@gmail.com", true);
-console.log(result); // { name: 'John', email: '
+// console.log(result); // { name: 'John', email: '
+
+const login = loginUser("a", "a@gmail.com");
+// console.log(login); // undefined
+
+
+
+
+// part-2
+
+
+
+
+
+
+
+
 export {};
