ded44c93cddb9fc48a35744dc077d50f93ab7484
abhisk1 <abhishekskarabani23@gmail.com>
2025-05-27 13:35:30 +0000

    delete all

--- dist/01_typescript/09_Tuples.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// const user: (string | number)[] = ["Ab", 1];
-Object.defineProperty(exports, "__esModule", { value: true });
-let tupleUser;
-tupleUser = [1, "ab", false]; // maintain order of the tuple data types.
-// tupleUser = ['hi', 2, true]; // Error: Type 'string' is not assignable to type 'number'.
-let rgb = [69, 69, 69];
-const newUser = [222, "AB@gmail.com"];
-newUser[1] = "ab@c.com"; // valid
