4d3c89d4395d35b1772b6a7173c4358d92b6b427
abhisk1 <abhishekskarabani23@gmail.com>
2025-05-30 13:26:23 +0000

    generics and generics class

---
+++ 01_typescript/17_generics.ts
@@ -0,0 +1,50 @@
+/* 01.Generics allow you to write reusable code, flexible, and type-safe code that works with different data types without losing the benefits of typescript type checking. */
+
+const score: Array<number> = [];
+const names: Array<String> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+} // not good
+
+function identityTwo(val: any): any {
+  return val;
+} // not good
+
+function identityThree<Type>(vale: Type): Type {
+  return vale;
+}
+
+// identityThree(true)
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+interface Bootle{
+  brand: string,
+  type: number
+}
+
+identityFour<Bootle>({return{
+  brand: 'Nike',
+  type: 1
+}})
+
+
+/* more about generics in arrray */
+
+function getSearchProduct<T>(products: T[]): T{
+  //do some DB operations.
+  const myIndex = 3;
+  return products[3];
+}
+
+// Arrow functions(peoper describing the function in Generics.)
+
+const getMoreSearchProducts = <T,>(products: t[]): T =>{
+  //do some DB operations.
+  const myIndex = 4;
+  return products[myIndex];
+}
+
